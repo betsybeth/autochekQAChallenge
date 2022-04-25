2d4974f2c99b6df9943ab5ffdf1897eb7cf55c7f
betsybeth <betsywambui@betsys-MacBook-Pro.local>
2022-04-25 14:15:08 +0300

    setup CI

--- cypress/integration/auth/verify_create_account_details.js
+++ cypress/integration/auth/verify_create_account_details.js
@@ -1,24 +1,22 @@
 /// <reference types="cypress" />
 
-import { And, Given, Then, When  } from "cypress-cucumber-preprocessor/steps";
+import { Given } from "cypress-cucumber-preprocessor/steps";
 
 Given("I visit autocheck url", function() {
-  cy.visit(
-    
-  ).url();
+  cy.visit().url();
 });
 
 // When("When I click on navitem to change to Ng", function(){
-    
+
 // });
 
 // And("I click on the register navitem", function(){
-    
+
 // })
 // And ("I insert information to register", function(){
-    
+
 // })
 // // eslint-disable-next-line cucumber/async-then
 // Then("I should register to the application", function(){
-    
+
 // })
--- cypress/pageObjects/auth.js
+++ cypress/pageObjects/auth.js
@@ -1,7 +1,9 @@
 class AuthPage {
-  main
-  ChooseCountryDropdownIcon = "div.main-container div.header.hide-:nth-child(2) div.topbar div.container div.topbar-location div.input-container.select-container.no-border div.input-icon-container.rightIcon span.input-icon.right > svg:nth-child(1)"
-  chooseNigeriaOption = "div.main-container div.header.hide-:nth-child(2) div.topbar div.container div.topbar-location div.input-container.select-container.no-border.isFocused ul.select-items > li:nth-child(1)"
+  main;
+  ChooseCountryDropdownIcon =
+    "div.main-container div.header.hide-:nth-child(2) div.topbar div.container div.topbar-location div.input-container.select-container.no-border div.input-icon-container.rightIcon span.input-icon.right > svg:nth-child(1)";
+  chooseNigeriaOption =
+    "div.main-container div.header.hide-:nth-child(2) div.topbar div.container div.topbar-location div.input-container.select-container.no-border.isFocused ul.select-items > li:nth-child(1)";
   registerNavItem = "register";
   SigninNavItem = "Sign In";
   firstNameInputField = ".social-buttons > :nth-child(1) > div > .input";
@@ -12,27 +14,27 @@ class AuthPage {
   registerButton = ".button";
   loginInButton = ".button";
 
-  fillFirstAndLastName(firstName, lastName){
-      cy.get(this.firstNameInputField)
+  fillFirstAndLastName(firstName, lastName) {
+    cy.get(this.firstNameInputField)
       .clear()
-      .type(firstName)
-      cy.get(this.LastNameInputField)
+      .type(firstName);
+    cy.get(this.LastNameInputField)
       .clear()
-      .type(lastName)
-    }
-  fillEmailAndPhone(email, phone){
-      cy.get(this.emailAddressInputField)
-      .clear()
-      .type(email)
-      cy.get(this.PhoneNumberInputField)
-      .clear()
-      .type(phone)  
+      .type(lastName);
   }
-  completeRegistrationFlow(){
-    this.fillFirstAndLastName()
+  fillEmailAndPhone(email, phone) {
+    cy.get(this.emailAddressInputField)
+      .clear()
+      .type(email);
+    cy.get(this.PhoneNumberInputField)
+      .clear()
+      .type(phone);
+  }
+  completeRegistrationFlow() {
+    this.fillFirstAndLastName();
     this.fillEmailAndPhone();
     cy.get(this.registerButton)
-    .should("exist")
-    .click()
+      .should("exist")
+      .click();
   }
 }
