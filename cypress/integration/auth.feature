94b4b7996c92c2a90ff7b72cb9d0b90c41f9e46e
betsy <bethwambuimuniu@gmail.com>
2022-04-25 14:05:22 +0300

    Revert "setup CI"

--- cypress/integration/auth.feature
+++ cypress/integration/auth.feature
@@ -1,8 +1,5 @@
 Feature: Authorization of a user 
   Scenario: Create an account 
     Given I visit autocheck main page
-    When I click on navitem to change to Ng
-    And I click on the register navitem
-    And I insert information to register 
-    Then I should register to the application
-
+    When I click on navitem to change to Nigeria 
+    
---
+++ cypress/integration/pageObjects/auth.js
@@ -0,0 +1,11 @@
+class AuthPage {
+  registerNavItem = "register";
+  SigninNavItem = "Sign In";
+  firstNameInputField = ".social-buttons > :nth-child(1) > div > .input";
+  LastNameInputField = ".social-buttons > :nth-child(2) > div > .input";
+  PhoneNumberInputField = ".form-control";
+  emailAddressInputField = ".form-content > :nth-child(2) > div > .input";
+  passwordInputField = ".relative > .input-container > div > .input";
+  registerButton = ".button";
+  loginInButton = ".button";
+}
